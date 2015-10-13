5d3ecb0366b1941f55034ab433d9b5c0e6309e05
Mate Peter <matepeter90@gmail.com>
2015-10-13 21:23:38 +0200

    Move transparent ftp proxy with virus scanning to the policy.

---
+++ transparent_ftp_clamav/policy.py
@@ -0,0 +1,36 @@
+##
+## Authors: Szilárd Pfeiffer <szilard.pfeiffer@balabit.com>
+##          Tibor Balázs <tibor.balazs@balabit.com>
+##
+## Permission is granted to copy, distribute and/or modify this document
+## under the terms of the GNU Free Documentation License, Version 1.3
+## or any later version published by the Free Software Foundation;
+## with no Invariant Sections, no Front-Cover Texts, and no Back-Cover Texts.
+## A copy of the license is included in the section entitled "GNU
+## Free Documentation License".
+##
+#############################################################################
+
+from Zorp.Core import *
+
+from Zorp.Ftp import *
+
+from zones import *
+
+class FtpProxyStackClamav(FtpProxy):
+    def config(self):
+        FtpProxy.config(self)
+        self.request_stack["RETR"]=(FTP_STK_DATA, (Z_STACK_PROGRAM, '/etc/zorp/scripts/clamav_stack.py'))
+
+def stack_instance():
+    Service(name="service_ftp_transparent_stack_clamav",
+        proxy_class=FtpProxyStackClamav,
+        router=TransparentRouter()
+    )
+
+    Rule(service='service_ftp_transparent_stack_clamav',
+         dst_port=21,
+         src_zone=('clients', ),
+         dst_zone=('servers.stack_clamav', )
+    )
+
