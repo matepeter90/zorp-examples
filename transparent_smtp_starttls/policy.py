166ef99f71fbbf374dad05b608bd6de4b4cdfd6d
Mate Peter <matepeter90@gmail.com>
2015-10-13 20:50:26 +0200

    Move transparent smtp proxy with starttls support to the policy.

---
+++ transparent_smtp_starttls/policy.py
@@ -0,0 +1,44 @@
+##
+## Authors: Szilárd Pfeiffer <szilard.pfeiffer@balabit.com>
+##          Tibor Balázs <tibor.balazs@balabit.com>
+##
+## Permission is granted to copy, distribute and/or modify this document
+## under the terms of the GNU Free Documentation License, Version 1.3
+## or any later version published by the Free Software Foundation;
+## with no Invariant Sections, no Front-Cover Texts, and no Back-Cover Texts.
+## A copy of the license is included in the section entitled "GNU
+## Free Documentation License".
+##
+#############################################################################
+
+from Zorp.Core import *
+from Zorp.Proxy import *
+
+from Zorp.Smtp import *
+from Zorp.Pop3 import *
+
+from zones import *
+
+class SmtpProxyStartTls(SmtpProxy):
+    def config(self):
+        SmtpProxy.config(self)
+        self.relay_zones=("*",)
+        self.ssl.client_connection_security = SSL_ACCEPT_STARTTLS
+        self.ssl.client_verify_type = SSL_VERIFY_OPTIONAL_UNTRUSTED
+        self.ssl.client_keypair_files=(
+                           "/etc/ssl/certs/ssl-cert-snakeoil.pem",
+                           "/etc/ssl/private/ssl-cert-snakeoil.key"
+                                              )
+        self.ssl.server_verify_type = SSL_VERIFY_OPTIONAL_UNTRUSTED
+
+def zorp_instance():
+    Service(name="service_smtp_transparent_starttls",
+        proxy_class=SmtpProxyStartTls,
+        router=TransparentRouter()
+    )
+
+    Rule(service='service_smtp_transparent_starttls',
+         dst_port=25,
+         src_zone=('clients'),
+         dst_zone=('servers.smtp_starttls')
+    )
